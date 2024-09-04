cb3dbdcc95eaa75ad3180ca034c6d81fb6d17fd5
Erik Costa <105504700+ErikCosta-O@users.noreply.github.com>
2024-09-04 00:32:29 -0300

    att

---
+++ assets/js/back.js
@@ -0,0 +1,12 @@
+function claro (){
+    const fundo = document.getElementById('fundoSite')
+        fundo.addEventListener('click' , fundo.style.backgroundColor ='#fffcf2', 
+            fundo.style.color='#000000'
+        )
+}
+function escuro(){
+    const fundo = document.getElementById('fundoSite')
+        fundo.addEventListener('click' , fundo.style.backgroundColor ='#1e1b18' ,
+            fundo.style.color ='#fffaff'
+        )
+}
